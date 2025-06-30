c77ee507e26cea36c383b21e1a016db1b7ed7181
Eiriksol <78737855+Eiriksol@users.noreply.github.com>
2025-06-30 13:03:11 +0000

    Add gardners equations

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -1,2 +1,14 @@
+import numpy as np
+
 def find_average(liste) -> float:
     return sum(liste) / len(liste)
+
+def gardners_equation(velocity) -> float:
+    alpha = 0.31
+    beta = 0.25
+    return alpha*(velocity**beta)
+
+def inverse_gardners_equation(density) -> float:
+    alpha = 0.31
+    beta = 0.25
+    return (density**(1/beta))/alpha
